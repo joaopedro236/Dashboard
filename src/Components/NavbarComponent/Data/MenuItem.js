50a6c79fa3209f01d3cf3cb2233ff50c5616b41d
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-06-06 00:36:58 -0300

    Refactor: Simplification of the Navbar component

---
+++ src/Components/NavbarComponent/Data/MenuItem.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        id:1,
+        title:'Dashboard',
+        icon:'https://img.icons8.com/?size=100&id=Yj5svDsC4jQA&format=png&color=973FFF'
+    },
+    {
+        id:2,
+        title:'Warnings',
+        icon:'https://img.icons8.com/?size=100&id=876&format=png&color=973FFF'
+    },
+    {
+        id:3,
+        title:'GitHub',
+        icon:'https://img.icons8.com/?size=100&id=12598&format=png&color=973FFF'
+    },
+    {
+        id:4,
+        title:'View Files',
+        icon: 'https://img.icons8.com/?size=100&id=112702&format=png&color=973FFF'
+    }
+]
--- src/Components/NavbarComponent/Navbar.jsx
+++ src/Components/NavbarComponent/Navbar.jsx
@@ -1,4 +1,5 @@
 import './Styles/Navbar.css'
+import { MenuItems } from './Data/MenuItem'
 import { useState,useEffect } from 'react'
 export default function Navbar() {
     const [NavbarActive, setNavbarActive] = useState(false)
@@ -24,22 +25,15 @@ export default function Navbar() {
         <section className={`menu  ${MenuActive ? 'Active' : ''}`}>
             <button className='closeMenu' onClick={()=> setMenuActive(false)}>X</button>
             <ul>
-                <li className={`${LiActive === 0 ? 'Active' : ''}`} onClick={()=> setLiActive(0)}>
-                    <img src="https://img.icons8.com/?size=100&id=Yj5svDsC4jQA&format=png&color=973FFF" alt="Icon Dashboard" className={`${LiActive === 0 ? 'Active' : ''}`}/>
-                    <p>Dashboard</p>
-                </li>
-                <li className={`${LiActive === 1 ? 'Active' : ''}`} onClick={()=> setLiActive(1)}>
-                    <img src="https://img.icons8.com/?size=100&id=876&format=png&color=973FFF" alt="Icon Warning" className={`${LiActive === 1 ? 'Active' : ''}`}/>
-                    <p>Warnings</p>
-                </li>
-                <li className={`${LiActive === 2 ? 'Active' : ''}`} onClick={()=> setLiActive(2)}>
-                    <img src="https://img.icons8.com/?size=100&id=12598&format=png&color=973FFF" alt="Icon GitHub" className={`${LiActive === 2 ? 'Active' : ''}`}/>
-                    <p>Github</p>
-                </li>
-                <li className={`${LiActive === 3 ? 'Active' : ''}`} onClick={()=> setLiActive(3)}>
-                    <img src="https://img.icons8.com/?size=100&id=112702&format=png&color=973FFF" alt="Icon Files" className={`${LiActive === 3 ? 'Active' : ''}`}/>
-                    <p>View Files</p>
-                </li>
+                {
+                    MenuItems.map((MenuItemsMap, index) => (
+                        <li key={index} className={`${LiActive === index ? 'Active' : ''}`}  onClick={()=> setLiActive(index)}>
+                            <img src={MenuItemsMap.icon} alt="Icons" className={`${LiActive === index ? 'Active' : ''}`}/>
+                            <p>{MenuItemsMap.title}</p>
+                        </li>
+                    ))
+                }
+
             </ul>
         </section>
         </>
